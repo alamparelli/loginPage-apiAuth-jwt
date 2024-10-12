e2be12ae4de2f69ca53d25be84e0299a1738761b
Alessandro <5610813+alamparelli@users.noreply.github.com>
2024-10-12 18:57:00 +0200

    added other DB instructions for settings and roles (setting will be filled later)

--- backend/controllers/database.js
+++ backend/controllers/database.js
@@ -23,7 +23,7 @@ export const addUser = (username, password, role) => {
 		'INSERT INTO user (username, password, role, created_at ) VALUES (?, ?, ?,?)',
 	);
 	insert.run(username, hash, role, Date.now());
-	return { succes: `${username} added in the DB` };
+	return { status: `${username} added in the DB` };
 };
 
 export const queryUser = (username, password) => {
@@ -44,4 +44,64 @@ export const queryUser = (username, password) => {
 export const queryAll = () => {
 	const query = database().prepare('SELECT * FROM user ORDER BY id DESC');
 	return query.all();
+};
+
+export const setPicture = (image) => {
+	// record the image as blob in the db
+	try {
+		fs.readFile(image, (err, data) => {
+			if (err) throw err;
+			const base64Image = data.toString('base64');
+
+			const insert = database().prepare(
+				'INSERT INTO user (profilePicture) VALUES (?) WHERE username = ?',
+			);
+			insert.run(base64Image, username);
+			return { status: `${username} added in the DB` };
+		});
+	} catch (error) {
+		return error.message;
+	}
+};
+
+export const getPicture = (username) => {
+	//retrieve image for the user
+	try {
+		const query = database().prepare(
+			'SELECT profilePicture FROM user WHERE username = ?',
+		);
+		const user = query.get(username);
+		if (user) {
+			return user.profilePicture; //in Base64Image
+		} else {
+			return false;
+		}
+	} catch (error) {
+		return error.message;
+	}
+};
+
+export const getRole = (username) => {
+	//retrieve the user role
+	try {
+		const query = database().prepare(
+			'SELECT role FROM user WHERE username = ?',
+		);
+		const user = query.get(username);
+		if (user) {
+			return user.role;
+		} else {
+			return false;
+		}
+	} catch (error) {
+		return error.message;
+	}
+};
+
+export const setSetting = (settingName, value, username) => {
+	//record a setting
+};
+
+export const getSetting = (settingName, username) => {
+	//retrieve a specific settings
 };
